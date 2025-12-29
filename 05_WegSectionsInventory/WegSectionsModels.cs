8bd6784c5ba53746f521f9dc2f4b7f231651aead
Nicholas Petree <petree234@gmail.com>
2025-12-28 22:12:28 -0600

    Created WegSectionsInventory to break sections down into appropriate fields

---
+++ 05_WegSectionsInventory/WegSectionsModels.cs
@@ -0,0 +1,22 @@
+﻿namespace OdinProjectAPI.Inventory;
+
+public sealed class WegSection
+{
+    public string? Name { get; set; }
+
+    public List<WegSectionProperty> Properties { get; set; } = new();
+
+    //List<T> is a reference type and would be null unless you initialized
+    //= new() creates an empty list by default, so the property is always usable.
+    public List<WegSection> Sections { get; set; } = new();
+
+}
+
+public sealed class WegSectionProperty
+{
+    public string Name { get; set; } = "";
+
+    public string Value { get; set; } = "";
+
+    public string Units { get; set; } = "";
+}
