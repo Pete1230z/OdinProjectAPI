f9dbc63376501287c18222226fe0e03d13266153
Nicholas Petree <petree234@gmail.com>
2025-12-29 19:38:44 -0600

    Minor adjustments

--- 02_Processing_Categories/WegCategoryModels.cs
+++ 02_Processing_Categories/WegCategoryModels.cs
@@ -23,6 +23,7 @@ public sealed class WegCategoryNode
     public string Variable { get; set; } = "";
     //Can be null
     public string? ParentVariable { get; set; }
+
     //List<T> is a reference type and would be null unless you initialized
     //= new() creates an empty list by default, so the property is always usable.
     public List<WegCategoryNode> Children { get; set; } = new();
